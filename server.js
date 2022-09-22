15cc2ad34a38e7fda0a6faa03058d5c3e3f2e4db
sumit sahni <3075sumitsahni@gmail.com>
2022-09-22 23:24:00 +0530

    update

--- server.js
+++ server.js
@@ -17,10 +17,10 @@ app.use('/public', express.static(path.join(__dirname, 'public')));
 
 
 if (process.env.NODE_ENV === 'production') {
-    app.use(express.static(path.join(__dirname, 'client/build')));
+    app.use(express.static(path.join(__dirname, 'frontend/build')));
     
     app.get('*', (req, res) => {
-        res.sendFile(path.join(__dirname, 'client/build/index.html'));
+        res.sendFile(path.join(__dirname, 'frontend/build/index.html'));
     });
 }else{
     app.get("/",(req,res)=>{
