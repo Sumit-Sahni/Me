8d0c689936cf4a8b9e65e04d27ddffd8096003f2
Sumit sahni <83834297+Sumit-Sahni@users.noreply.github.com>
2022-12-25 12:53:24 +0530

    Update server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 const cors = require('cors');
-const path = require('path');
+var   path = require('path');
 const dotenv = require('dotenv');
 
 dotenv.config();
