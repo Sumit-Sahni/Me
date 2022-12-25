28f6a179d47af479af049b136b1c666b6db8d01f
Sumit sahni <83834297+Sumit-Sahni@users.noreply.github.com>
2022-12-25 12:45:58 +0530

    Update server.js

--- server.js
+++ server.js
@@ -9,19 +9,20 @@ dotenv.config();
 app.use(cors());
 app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: false })); 
-app.use('/public', express.static(path.join(__dirname, 'public')));
+
 
 // app.get("/", (req, res)=>{
 //     res.send("Hello")
 // })
-
-app.use(express.static(path.join(__dirname, "./frontend/build")))
-     app.get('*', (req, res) => {
-          res.sendFile(path.join(__dirname, './frontend/build/index.html')),
-          function(err){
-             res.status(500).send(err);
-          }});
-     
+app.use(express.static(path.join(__dirname, "./frontend/build")));
+app.get("*", function (_, res) {
+  res.sendFile(
+    path.join(__dirname, "./frontend/build/index.html"),
+    function (err) {
+      res.status(500).send(err);
+    }
+  );
+});
 
 
 const PORT = process.env.PORT;  
